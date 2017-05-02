35afe2c46763c8e9539f392d82e32debe2e3d53c
adamhsparks <adamhsparks@gmail.com>
2017-05-03 06:39:05 +1000

    Add documentation for AAC_codes data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#'AAC_codes
+#'
+#' @format A data frame with 1369 observations of 4 elements:
+#' \describe{
+#'   \item{AAC}{Unique identifier for each location}
+#'   \item{PT_NAME}{Human readable location name}
+#'   \item{LON}{Longitude}
+#'   \item{LAT}{Latitude}
+#' }
+#'
+#' The \code{AAC_codes} data are automatically loaded with the
+#' \code{\link{BOMRang}} package and merged with the latest available forecast
+#' from the BOM.
+#'
+#'
+#' @source \url{ftp://ftp.bom.gov.au/anon/home/adfd/spatial/IDM00013.dbf}
+#'
+"AAC_codes"
