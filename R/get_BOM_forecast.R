5537a0500226ad2b132debb71a7c079664cdccab
Adam H. Sparks <adamhsparks@gmail.com>
2017-05-04 07:18:13 +1000

    Add functionality to fetch all state/territory forecasts

--- R/get_BOM_forecast.R
+++ R/get_BOM_forecast.R
@@ -4,6 +4,8 @@
 #'Fetch the BOM forecast and create a data frame object that can be used for
 #'interpolating.
 #'
+#' @param state Australian state or territory, either as full name or abbreviation
+#'
 #' @return
 #' Data frame of a Australia BOM forecast for Queensland for max temperature,
 #' min temperature and corresponding locations with lat/lon values for the next
@@ -11,94 +13,125 @@
 #'
 #' @examples
 #' \dontrun{
-#' BOM_forecast <- get_BOM_QLD()
+#' BOM_forecast <- get_forecast(state = "Qld")
 #' }
-#' @export
 #'
 #' @importFrom dplyr %>%
-get_BOM_QLD <- function() {
-
+#'
+#'
+#' @export
+get_forecast <- function(state) {
   # Load BOM location data
   utils::data("AAC_codes", package = "BOMRang")
   AAC_codes <- AAC_codes
 
-  # fetch BOM foreast for Qld
-  xmlforecast <-
-    xml2::read_xml("ftp://ftp.bom.gov.au/anon/gen/fwo/IDQ11295.xml")
+  # Select state/territory forecast and fetch
 
-  # remove index=0 (today's "forecast"), it varies and we're not interested anyway
-  xml2::xml_find_all(xmlforecast, ".//*[@index='0']") %>%
-    xml2::xml_remove()
-
-  # extract locations from forecast
-  areas <- xml2::xml_find_all(xmlforecast, ".//*[@type='location']")
-  forecast_locations <-
-    dplyr::bind_rows(lapply(xml2::xml_attrs(areas), as.list))
-
-  # join locations with lat/lon values for mapping and interpolation
-  forecast_locations <- dplyr::left_join(forecast_locations,
-                                         AAC_codes,
-                                         by = c("aac" = "AAC",
-                                                "description" = "PT_NAME"))
-
-    # unlist and add the locations aac code
-  forecasts <-
-    lapply(xml2::xml_find_all(xmlforecast, ".//*[@type='location']"),
-           xml2::as_list)
-
-  forecasts <- plyr::llply(forecasts, unlist)
-  names(forecasts) <- forecast_locations$aac
-
-  # get all the <element> and <text> tags (the forecast)
-  eltext <- xml2::xml_find_all(xmlforecast, "//element | //text")
-
-  # extract and clean (if needed) (the labels for the forecast)
-  labs <- trimws(xml2::xml_attrs(eltext, "type"))
-
-  # use a loop to turn list of named character elements into a list of dataframes
-  # with the location aac code for each line of the data frame
-  y <- vector("list")
-  for (i in unique(names(forecasts))) {
-    x <- data.frame(
-      keyName = names(forecasts[[i]]),
-      value = forecasts[[i]],
-      row.names = NULL
-    )
-    z <- names(forecasts[i])
-    x <- data.frame(rep(as.character(z), nrow(x)), x)
-    y[[i]] <- x
+  if (state = "NT") {
+    xmlforecast <-
+      xml2::read_xml("ftp://ftp.bom.gov.au/anon/gen/fwo/IDD10207.xml") # nt
   }
+  else if (state = "NSW") {
+    xmlforecast <-
+      xml2::read_xml("ftp://ftp.bom.gov.au/anon/gen/fwo/IDN11060.xml") # nsw
+  }
+  else if (state = "QLD") {
+    xmlforecast <-
+      xml2::read_xml("ftp://ftp.bom.gov.au/anon/gen/fwo/IDN11060.xml") # qld
+  }
+  else if (state = "SA") {
+    xmlforecast <-
+      xml2::read_xml("ftp://ftp.bom.gov.au/anon/gen/fwo/IDS10044.xml") # sa
+  }
+  else if (state = "TAS")
+  {
+    xmlforecast <-
+      xml2::read_xml("ftp://ftp.bom.gov.au/anon/gen/fwo/IDT16710.xml") # tas
+  }
+  else if (state = "VIC") {
+    xmlforecast <-
+      xml2::read_xml("ftp://ftp.bom.gov.au/anon/gen/fwo/IDV10753.xml") # vic
+  }
+  else if (state == "WA") {
+    xmlforecast <-
+      xml2::read_xml("ftp://ftp.bom.gov.au/anon/gen/fwo/IDW14199.xml") # wa
+  } else
+    stop(state, " not recognised as a valid state or territory")
 
-  # combind list into a single dataframe
-  y <- data.table::rbindlist(y, fill = TRUE)
 
-  # add the forecast description to the dataframe
-  forecast <- data.frame(y, labs, rep(NA, length(labs)))
-  names(forecast) <- c("aac", "keyName", "value", "labs", "element")
+# remove index=0 (today's "forecast"), it varies and we're not interested anyway
+xml2::xml_find_all(xmlforecast, ".//*[@index='0']") %>%
+  xml2::xml_remove()
 
-  # add dates to the new object
-  forecast$date <- c(rep(seq(
-    lubridate::ymd(Sys.Date() + 1),
-    lubridate::ymd(Sys.Date() + 7),
-    by = "1 day"
-  ),
-  each = 2))
+# extract locations from forecast
+areas <- xml2::xml_find_all(xmlforecast, ".//*[@type='location']")
+forecast_locations <-
+  dplyr::bind_rows(lapply(xml2::xml_attrs(areas), as.list))
 
-  # label for min/max temperature in a new col to use for sorting in next step
-  forecast$element <-
-    as.character(stringr::str_match(forecast$labs,
-                                    "air_temperature_[[:graph:]]{7}"))
+# join locations with lat/lon values for mapping and interpolation
+forecast_locations <- dplyr::left_join(forecast_locations,
+                                       AAC_codes,
+                                       by = c("aac" = "AAC",
+                                              "description" = "PT_NAME"))
 
-  # convert object to tibble and remove rows we don't need, e.g., precip
-  # keep only max and min temp
-  forecast <-
-    tibble::as_tibble(stats::na.omit(forecast[, c(1, 3, 5:6)]))
+# unlist and add the locations aac code
+forecasts <-
+  lapply(xml2::xml_find_all(xmlforecast, ".//*[@type='location']"),
+         xml2::as_list)
 
-  # convert forecast_locations$aac to factor for merging
-  forecast$aac <- as.character(forecast$aac)
+forecasts <- plyr::llply(forecasts, unlist)
+names(forecasts) <- forecast_locations$aac
 
-  # merge the forecast with the locations
-  forecast <-
-    dplyr::left_join(forecast, forecast_locations, by = "aac")
+# get all the <element> and <text> tags (the forecast)
+eltext <- xml2::xml_find_all(xmlforecast, "//element | //text")
+
+# extract and clean (if needed) (the labels for the forecast)
+labs <- trimws(xml2::xml_attrs(eltext, "type"))
+
+# use a loop to turn list of named character elements into a list of dataframes
+# with the location aac code for each line of the data frame
+y <- vector("list")
+for (i in unique(names(forecasts))) {
+  x <- data.frame(
+    keyName = names(forecasts[[i]]),
+    value = forecasts[[i]],
+    row.names = NULL
+  )
+  z <- names(forecasts[i])
+  x <- data.frame(rep(as.character(z), nrow(x)), x)
+  y[[i]] <- x
+}
+
+# combind list into a single dataframe
+y <- data.table::rbindlist(y, fill = TRUE)
+
+# add the forecast description to the dataframe
+forecast <- data.frame(y, labs, rep(NA, length(labs)))
+names(forecast) <- c("aac", "keyName", "value", "labs", "element")
+
+# add dates to the new object
+forecast$date <- c(rep(seq(
+  lubridate::ymd(Sys.Date() + 1),
+  lubridate::ymd(Sys.Date() + 7),
+  by = "1 day"
+),
+each = 2))
+
+# label for min/max temperature in a new col to use for sorting in next step
+forecast$element <-
+  as.character(stringr::str_match(forecast$labs,
+                                  "air_temperature_[[:graph:]]{7}"))
+
+# convert object to tibble and remove rows we don't need, e.g., precip
+# keep only max and min temp
+forecast <-
+  tibble::as_tibble(stats::na.omit(forecast[, c(1, 3, 5:6)]))
+
+# convert forecast_locations$aac to factor for merging
+forecast$aac <- as.character(forecast$aac)
+
+# merge the forecast with the locations
+forecast <-
+  dplyr::left_join(forecast, forecast_locations, by = "aac")
 
 }
