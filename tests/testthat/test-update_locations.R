464366f38423c9319e1011c012fe359dfa488efe
Adam H. Sparks <adamhsparks@gmail.com>
2017-05-12 14:08:19 +1000

    Add more tests for update_locations.R

--- tests/testthat/test-update_locations.R
+++ tests/testthat/test-update_locations.R
@@ -6,3 +6,22 @@ test_that("Timeout options are reset on update_locations() exit", {
   update_locations()
   expect_equal(options("timeout")[[1]], 60)
 })
+
+# update_locations() downloads and imports the proper file ---------------------
+
+test_that("update_locations() downloads and imports the proper file", {
+  skip_on_cran()
+  utils::download.file(
+    "ftp://ftp.bom.gov.au/anon/home/adfd/spatial/IDM00013.dbf",
+    destfile = paste0(tempdir(), "AAC_codes.dbf"),
+    mode = "wb"
+  )
+
+  # import BOM dbf file
+  AAC_codes <-
+    foreign::read.dbf(paste0(tempdir(), "AAC_codes.dbf"), as.is = TRUE)
+  AAC_codes <- AAC_codes[, c(2:3, 7:9)]
+
+  expect_equal(ncol(AAC_codes), 5)
+  expect_named(AAC_codes, c("AAC", "PT_NAME", "LON", "LAT", "ELEVATION"))
+})
