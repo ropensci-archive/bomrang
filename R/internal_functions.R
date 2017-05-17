a6e618f630d773a8fdcc7588a19b333381d23900
HughParsonage <hugh.parsonage@gmail.com>
2017-05-17 22:38:09 +1000

    Add utility functions

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -6,3 +6,9 @@
     } else
       stop("\nPlease provide a valid 2 or 3 letter state or territory postal code abbreviation")
   }
+
+`%notin%` <- Negate("%in%")
+
+force_double <- function(v) {
+  suppressWarnings(as.double(v))
+}
