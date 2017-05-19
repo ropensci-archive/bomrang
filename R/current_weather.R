b3476ad3b21e497055f6d1568aeb9d8518d8eb3f
HughParsonage <hugh.parsonage@gmail.com>
2017-05-20 01:32:27 +1000

    Link to sweep_for_stations

--- R/current_weather.R
+++ R/current_weather.R
@@ -3,7 +3,8 @@
 #' @param station_name The name of the weather station. Fuzzy string matching via
 #' \code{base::agrep} is done.
 #' @param latlon A length-2 numeric vector. When given instead of \code{station_name}, the nearest station
-#' (in this package) is used, with a message indicating the nearest such station. Ignored if used in combination with
+#' (in this package) is used, with a message indicating the nearest such station. (See also \code{\link{sweep_for_stations}}.)
+#' Ignored if used in combination with
 #' \code{station_name}, with a warning.
 #' @param raw Do not convert the columns \code{data.table} to the appropriate classes. (\code{FALSE} by default.)
 #' @param emit_latlon_msg Logical. If \code{TRUE} (the default), and \code{latlon} is selected
