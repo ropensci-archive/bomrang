f20a94044b5acca926c201d2e72f826595c52a09
HughParsonage <hugh.parsonage@gmail.com>
2017-05-20 00:38:43 +1000

    Initial commit and tests of haversine distance
    Great circle distance function. Vectorized.

--- R/internal_functions.R
+++ R/internal_functions.R
@@ -12,3 +12,24 @@
 force_double <- function(v) {
   suppressWarnings(as.double(v))
 }
+
+# Distance over a great circle. Reasonable approximation.
+haversine_distance <- function(lat1, lon1, lat2, lon2) {
+  # to radians
+  lat1 <- lat1 * pi / 180
+  lat2 <- lat2 * pi / 180
+  lon1 <- lon1 * pi / 180
+  lon2 <- lon2 * pi / 180
+
+  delta_lat <- abs(lat1 - lat2)
+  delta_lon <- abs(lon1 - lon2)
+
+  # radius of earth
+  6371 * 2 * asin(
+    sqrt(
+      `+`((sin(delta_lat / 2)) ^ 2,
+          cos(lat1) * cos(lat2) * (sin(delta_lon / 2)) ^ 2
+      )
+    )
+  )
+}
