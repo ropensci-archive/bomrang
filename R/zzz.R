f011f559298dd1524847d3d77ac56d5ab5005c56
adamhsparks <adamhsparks@gmail.com>
2017-06-02 07:10:15 +1000

    Load databases on package load

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,8 @@
     # CRAN Note avoidance
     if (getRversion() >= "2.15.1") {
       utils::globalVariables(c("."))
+
+      data("AAC_codes", "stations_site_list", "JSONurl_latlon_by_station_name",
+           package = pkgname, envir = parent.env(environment()))
     }
   }
