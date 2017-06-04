7f7aedb3abf39d1bab9f92e68efc8d10b90a2f89
adamhsparks <adamhsparks@gmail.com>
2017-06-04 14:08:40 +1000

    Fix missing comma in documentation

--- R/get_precis_forecast.R
+++ R/get_precis_forecast.R
@@ -30,7 +30,7 @@
 #' @examples
 #' BOM_forecast <- get_precis_forecast(state = "QLD")
 #'
-#' @author Adam H Sparks, \email{adamhsparks@gmail.com} and Keith Pembleton \email{keith.pembleton@usq.edu.au}
+#' @author Adam H Sparks, \email{adamhsparks@gmail.com} and Keith Pembleton, \email{keith.pembleton@usq.edu.au}
 #'
 #' @references
 #' Australian Bureau of Meteorology (BOM) Weather Data Services
