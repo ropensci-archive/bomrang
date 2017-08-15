8af39b71bcf965ad1aee01ec1a54eaf4ed5aafbc
adamhsparks <adamhsparks@gmail.com>
2017-08-16 07:01:49 +1000

    Add rgdal as depends; fix bug with product_id limits
    - When selecting GeoTIFFs for download, only one `product_id` is
    supported, this fixes a bug in the initial check that is performed
    
    - Add _rgdal_ to Depends in DESCRIPTION since it is required to read
    the GeoTIFF files as a RasterStack object

--- R/get_satellite_imagery.R
+++ R/get_satellite_imagery.R
@@ -145,7 +145,7 @@ get_satellite_imagery <-
     if (is.null(product_id)) {
       stop("\nYou must select a valid BoM satellite imagery Product ID.\n")
     }
-    if (length(product_id > 1)) {
+    if (length(unique(substr(product_id, 1, 8))) != 1) {
       stop("/nbomrang only supports working with one Product ID at a time\n")
     }
 
