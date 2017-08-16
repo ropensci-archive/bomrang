c7277de85d321a07b98f4d9967b29151731e017c
adamhsparks <adamhsparks@gmail.com>
2017-08-16 22:45:00 +1000

    Fix another errant call to the wrong package
    It’s late, I’m tired and sloppy

--- tests/testthat/test-check_cache.R
+++ tests/testthat/test-check_cache.R
@@ -1,7 +1,7 @@
 
 context("Cache directory handling")
 
-# test that .set_cache creates a cache directory if none exists ----------------
+# test that .set_cache() creates a cache directory if none exists --------------
 test_that("test that set_cache creates a cache directory if none exists", {
   skip_on_cran()
   unlink(rappdirs::user_cache_dir("bomrang"), recursive = TRUE)
@@ -13,13 +13,14 @@ test_that("test that set_cache creates a cache directory if none exists", {
                                   appauthor = "bomrang"))
 })
 
-# test that .set_cache does a cache directory if cache is FALSE ----------------
+# test that .set_cache() does a cache directory if cache is FALSE --------------
 
 test_that("test that set_cache does not create a dir if cache == FALSE", {
   cache <- FALSE
   cache_dir <- .set_cache(cache)
   expect_true(cache_dir == tempdir())
 })
+
 
 test_that("cache directory is created if necessary", {
   # if cache directory exists during testing, remove it
@@ -34,6 +35,8 @@ test_that("cache directory is created if necessary", {
   ))
 })
 
+# test that file lists and deletions are properly handled ----------------------
+
 test_that("caching utils list files in cache and delete when asked", {
   skip_on_cran()
   unlink(rappdirs::user_cache_dir(appname = "bomrang",
@@ -44,19 +47,19 @@ test_that("caching utils list files in cache and delete when asked", {
   raster::writeRaster(f, file.path(cache_dir, "file1.tif"), format = "GTiff")
   raster::writeRaster(f, file.path(cache_dir, "file2.tif"), format = "GTiff")
 
-  # test getCRUCLdata cache list
+  # test bomrang cache list
   k <- list.files(rappdirs::user_cache_dir(appname = "bomrang",
                                            appauthor = "bomrang"))
-  expect_equal(basename(CRU_cache_list()), k)
+  expect_equal(basename(bomrang_cache_list()), k)
 
   # test delete one file
-  CRU_cache_delete("file1.tif")
+  bomrang_cache_delete("file1.tif")
   l <- list.files(rappdirs::user_cache_dir(appname = "bomrang",
                                            appauthor = "bomrang"))
-  expect_equal(basename(CRU_cache_list()), l)
+  expect_equal(basename(bomrang_cache_list()), l)
 
   # test delete all
-  CRU_cache_delete_all()
+  bomrang_cache_delete_all()
   expect_equal(list.files(rappdirs::user_cache_dir(appname = "bomrang",
                                                    appauthor = "bomrang")
   ),
