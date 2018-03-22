c60843a4cfe7d645a0e8b5bd6cfa5030c9e90e33
Adam H. Sparks <adamhsparks@gmail.com>
2018-03-22 11:39:26 +1000

    Update test for sweep_for_stations

--- tests/testthat/test-sweep_for_stations.R
+++ tests/testthat/test-sweep_for_stations.R
@@ -3,5 +3,5 @@ context("Sweep for stations")
 test_that("sweep_for_stations returns correct default", {
   DT <- sweep_for_stations()
   expect_equal(DT$name[1], "CANBERRA AIRPORT")
-  expect_equal(data.table::last(DT$name), "LEARMONTH AIRPORT")
+  expect_equal(data.table::last(DT$name), "MAWSON")
 })
