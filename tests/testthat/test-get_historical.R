4d254e5ea6ded5f24a983f6357d9057ee837d2dd
Jonathan Carroll <jono@jcarroll.com.au>
2018-04-18 00:10:01 +0930

    retrieve historical observations.
    added NEWS item to potential v0.1.5.
    
    added @jonocarroll as author.
    
    added tests with 100% coverage.
    
    no additional dependencies.
    
    no significant NOTEs.
    
    version requires bump in DESCRIPTION.

---
+++ tests/testthat/test-get_historical.R
@@ -0,0 +1,29 @@
+context("Historical Observations")
+
+test_that("Error handling", {
+  expect_error(get_historical(), regexp = "stationid.*latlon.*provided")
+  expect_error(get_historical("sodiuhfosdhfoisdh"), regexp = "Station not recognised")
+  expect_error(get_historical(1), regexp = "Station not recognised.")
+  expect_error(get_historical("1"), regexp = "Station not recognised.")
+  expect_error(get_historical("023001"), regexp = "Station not recognised.")
+  expect_error(get_historical(latlon = 1), regexp = "2-element")
+  expect_error(get_historical(latlon = c("a", "b")), regexp = "2-element")
+  expect_warning(get_historical("023000", c(1, 2)), regexp = "Only one.*stationid.*latlon")
+  expect_error(get_historical("023000", type = "sodiuhfosdhfoisdh"), regexp = "arg.*rain.*solar")
+})
+
+test_that("Query stationid = '023000', type = 'rain' returns data.frame w/ correct station and some data", {
+  ADLhistrain <- get_historical("023000", type = "rain")
+  expect_is(ADLhistrain, "data.frame")
+  expect_true(nrow(ADLhistrain) > 0)
+  expect_equal(ADLhistrain$Product.code[1], factor("IDCJAC0009"))
+  expect_equal(ADLhistrain$Bureau.of.Meteorology.station.number[1], 23000)
+})
+
+test_that("Query latlon = c(-34.9285, 138.6007), type = 'rain' returns data.frame w/ correct station and some data", {
+  ADLhistrain <- get_historical(latlon = c(-34.9285, 138.6007), type = "rain")
+  expect_is(ADLhistrain, "data.frame")
+  expect_true(nrow(ADLhistrain) > 0)
+  expect_equal(ADLhistrain$Product.code[1], factor("IDCJAC0009"))
+  expect_equal(ADLhistrain$Bureau.of.Meteorology.station.number[1], 23000)
+})
