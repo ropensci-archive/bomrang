5bc439bf30d8a178b3348736970a23f45bbc0c32
Adam H. Sparks <adamhsparks@gmail.com>
2018-04-22 09:36:33 +1000

    Line lengths

--- tests/testthat/test-get_historical.R
+++ tests/testthat/test-get_historical.R
@@ -2,14 +2,17 @@ context("Historical Observations")
 
 test_that("Error handling", {
   expect_error(get_historical(), regexp = "stationid.*latlon.*provided")
-  expect_error(get_historical("sodiuhfosdhfoisdh"), regexp = "Station not recognised")
+  expect_error(get_historical("sodiuhfosdhfoisdh"),
+               regexp = "Station not recognised")
   expect_error(get_historical(1), regexp = "Station not recognised.")
   expect_error(get_historical("1"), regexp = "Station not recognised.")
   expect_error(get_historical("023001"), regexp = "Station not recognised.")
   expect_error(get_historical(latlon = 1), regexp = "2-element")
   expect_error(get_historical(latlon = c("a", "b")), regexp = "2-element")
-  expect_warning(get_historical("023000", c(1, 2)), regexp = "Only one.*stationid.*latlon")
-  expect_error(get_historical("023000", type = "sodiuhfosdhfoisdh"), regexp = "arg.*rain.*solar")
+  expect_warning(get_historical("023000", c(1, 2)),
+                 regexp = "Only one.*stationid.*latlon")
+  expect_error(get_historical("023000", type = "sodiuhfosdhfoisdh"),
+               regexp = "arg.*rain.*solar")
 })
 
 test_that("Query stationid = '023000', type = 'rain' returns data.frame w/ correct station and some data", {
@@ -29,7 +32,8 @@ test_that("Query latlon = c(-34.9285, 138.6007), type = 'rain' returns data.fram
 })
 
 test_that("Zip file URL is correctly obtained", {
-  expect_error(bomrang:::.get_zip_url("023001", 122), regexp = "resource identifiers")
+  expect_error(bomrang:::.get_zip_url("023001", 122),
+               regexp = "resource identifiers")
   expect_silent(ADLzipURL <- bomrang:::.get_zip_url("023000", 122))
   expect_match(ADLzipURL, "-105815575")
 })
