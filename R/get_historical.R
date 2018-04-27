c6f7a3f2cdef890e83e7a94360047fbda3e33aa8
Jonathan Carroll <jono@jcarroll.com.au>
2018-04-27 19:49:22 +0930

    Field names in get_historical now use _ rather than . and are documented. Closes #69.

--- R/get_historical.R
+++ R/get_historical.R
@@ -6,11 +6,38 @@
 #' @md
 #' @param stationid BOM station ID. See Details.
 #' @param latlon Length-2 numeric vector of Latitude/Longitude. See Details.
-#' @param type Measurement type, either daily "rain", "min" (temp), "max" (temp), or
-#'   "solar" (exposure). Partial matching is performed.
+#' @param type Measurement type, either daily "rain", "min" (temp), "max"
+#'   (temp), or "solar" (exposure). Partial matching is performed.
 #'
 #' @return a complete [data.frame] of historical observations for the chosen
-#'   station.
+#'   station, with some subset of the following columns
+#'
+#'   \tabular{rl}{
+#'   **Product_code**:\tab BOM internal code.\cr
+#'   **Station_number**:\tab BOM station ID.\cr
+#'   **Year**:\tab Year of observation (YYYY).\cr
+#'   **Month**:\tab Month of observation (1-12).\cr
+#'   **Day**:\tab Day of observation (1-31).\cr
+#'   **Min_temperature**:\tab Minimum daily recorded temperature (degrees C).\cr
+#'   **Max_temperature**:\tab Maximum daily recorded temperature (degrees C).\cr
+#'   **Accum_days_min**:\tab Accumulated number of days of minimum temperature.\cr
+#'   **Accum_days_max**:\tab Accumulated number of days of maximum temperature.\cr
+#'   **Rainfall**:\tab Daily recorded rainfall in mm.\cr
+#'   **Period**:\tab Period over which rainfall was measured.\cr
+#'   **Solar_exposure**:\tab Daily global solar exposure in MJ/m^2.\cr
+#'   **Quality**:\tab Y, N, or missing. Data which have not yet completed the
+#'   routine quality control process are marked accordingly.
+#'   }
+#'
+#'   Temperature data prior to 1910 should be used with extreme caution as many
+#'   stations, prior to that date, were exposed in non-standard shelters, some
+#'   of which give readings which are several degrees warmer or cooler than
+#'   those measured according to post-1910 standards.
+#'
+#'   Daily maximum temperatures usually occur in the afternoon and daily minimum
+#'   temperatures overnight or near dawn. Occasionally, however, the lowest
+#'   temperature in the 24 hours to prior to 9 am can occur around 9 am the
+#'   previous day if the night was particularly warm.
 #'
 #'   Either `stationid` or `latlon` must be provided, but if both are, then
 #'   `stationid` will be used as it is more reliable.
@@ -71,6 +98,39 @@ get_historical <-
     )
 
     zipurl <- .get_zip_url(stationid, obscode)
-    .get_zip_and_load(zipurl)
-
+    dat <- .get_zip_and_load(zipurl)
+    
+    names(dat) <- switch(type, 
+                         min = c("Product_code", 
+                                 "Station_number", 
+                                 "Year", 
+                                 "Month", 
+                                 "Day", 
+                                 "Min_temperature", 
+                                 "Accum_days_min", 
+                                 "Quality"),
+                         max = c("Product_code", 
+                                 "Station_number", 
+                                 "Year", 
+                                 "Month", 
+                                 "Day", 
+                                 "Max_temperature", 
+                                 "Accum_days_max", 
+                                 "Quality"),
+                         rain = c("Product_code", 
+                                  "Station_number", 
+                                  "Year", 
+                                  "Month", 
+                                  "Day", 
+                                  "Rainfall", 
+                                  "Period", 
+                                  "Quality"),
+                         solar = c("Product_code", 
+                                   "Station_number", 
+                                   "Year", 
+                                   "Month", 
+                                   "Day", 
+                                   "Solar_exposure")
+    )
+    dat
   }
