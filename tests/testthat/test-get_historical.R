e03dcbf9a3ab1630c63c6c753617e5d8f739831a
Adam H. Sparks <adamhsparks@gmail.com>
2018-05-05 19:13:51 +1000

    Correct field names in tests for get_historical()

--- tests/testthat/test-get_historical.R
+++ tests/testthat/test-get_historical.R
@@ -19,16 +19,16 @@ test_that("Query stationid = '023000', type = 'rain' returns data.frame w/ corre
   ADLhistrain <- get_historical("023000", type = "rain")
   expect_is(ADLhistrain, "data.frame")
   expect_true(nrow(ADLhistrain) > 0)
-  expect_equal(ADLhistrain$Product.code[1], factor("IDCJAC0009"))
-  expect_equal(ADLhistrain$Bureau.of.Meteorology.station.number[1], 23000)
+  expect_equal(ADLhistrain$Product_code[1], factor("IDCJAC0009"))
+  expect_equal(ADLhistrain$Station_number[1], 23000)
 })
 
 test_that("Query latlon = c(-34.9285, 138.6007), type = 'rain' returns data.frame w/ correct station and some data", {
   ADLhistrain <- get_historical(latlon = c(-34.9285, 138.6007), type = "rain")
   expect_is(ADLhistrain, "data.frame")
   expect_true(nrow(ADLhistrain) > 0)
-  expect_equal(ADLhistrain$Product.code[1], factor("IDCJAC0009"))
-  expect_equal(ADLhistrain$Bureau.of.Meteorology.station.number[1], 23000)
+  expect_equal(ADLhistrain$Product_code[1], factor("IDCJAC0009"))
+  expect_equal(ADLhistrain$Station_number[1], 23000)
 })
 
 test_that("Zip file URL is correctly obtained", {
