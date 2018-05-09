1433668714771f447ee76b61b9b8011a0efd8f55
Adam H. Sparks <adamhsparks@gmail.com>
2018-05-09 22:24:08 +1000

    Wrap long lines in test

--- tests/testthat/test-get_historical.R
+++ tests/testthat/test-get_historical.R
@@ -15,7 +15,8 @@ test_that("Error handling", {
                regexp = "arg.*rain.*solar")
 })
 
-test_that("Query stationid = '023000', type = 'rain' returns data.frame w/ correct station and some data", {
+test_that("Query stationid = '023000',
+          type = 'rain' returns data.frame w/ correct station and some data", {
   ADLhistrain <- get_historical("023000", type = "rain")
   expect_is(ADLhistrain, "data.frame")
   expect_true(nrow(ADLhistrain) > 0)
@@ -23,7 +24,8 @@ test_that("Query stationid = '023000', type = 'rain' returns data.frame w/ corre
   expect_equal(ADLhistrain$Station_number[1], 23000)
 })
 
-test_that("Query latlon = c(-34.9285, 138.6007), type = 'rain' returns data.frame w/ correct station and some data", {
+test_that("Query latlon = c(-34.9285, 138.6007),
+          type = 'rain' returns data.frame w/ correct station and some data", {
   ADLhistrain <- get_historical(latlon = c(-34.9285, 138.6007), type = "rain")
   expect_is(ADLhistrain, "data.frame")
   expect_true(nrow(ADLhistrain) > 0)
