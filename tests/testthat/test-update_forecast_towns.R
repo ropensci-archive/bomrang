c74938d8afac364d8facbb8dd730febcc741ad80
Adam H. Sparks <adamhsparks@gmail.com>
2018-08-26 22:17:23 +1000

    Add tests for new user input for updating internal DBs

--- tests/testthat/test-update_forecast_towns.R
+++ tests/testthat/test-update_forecast_towns.R
@@ -1,18 +1,30 @@
- context("update_forecast_locations")
 
- # Timeout options are reset on update_forecast_locations() exit ---------------
- test_that("Timeout options are reset on update_forecast_towns() exit", {
-   skip_on_cran()
-   update_forecast_towns()
-   expect_equal(options("timeout")[[1]], 60)
- })
+context("update_forecast_locations")
 
- # update_forecast_locations() downloads and imports the proper file -----------
- test_that("update_forecast_towns() downloads and imports proper file", {
-   skip_on_cran()
-   update_forecast_towns()
-   AAC_codes <- NULL
-   load(system.file("extdata", "AAC_codes.rda", package = "bomrang"))
-   expect_equal(ncol(AAC_codes), 5)
-   expect_named(AAC_codes, c("AAC", "PT_NAME", "LON", "LAT", "ELEVATION"))
- })
+# If input is no, don't proceed ------------------------------------------------
+test_that("update_forecast_towns() stops if 'no'", {
+  skip_on_cran()
+  
+  f <- file()
+  options(bomrang_connection = f)
+  ans <- "no"
+  write(ans, f)
+  expect_error(update_forecast_towns())
+  options(bomrang_connection = stdin())
+  close(f)
+})
+
+# update_forecast_locations() downloads and imports the proper file ------------
+test_that("update_forecast_towns() downloads and imports proper file", {
+  skip_on_cran()
+  f <- file()
+  options(bomrang_connection = f)
+  ans <- "yes"
+  write(ans, f)
+  update_forecast_towns()
+  AAC_codes <- NULL
+  load(system.file("extdata", "AAC_codes.rda", package = "bomrang"))
+  expect_equal(ncol(AAC_codes), 5)
+  expect_named(AAC_codes, c("AAC", "PT_NAME", "LON", "LAT", "ELEVATION"))
+  expect_equal(options("timeout")[[1]], 60)
+})
