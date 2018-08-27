bd91e4f9b4977498d1b4cb57821fdb1382e3a321
Adam H. Sparks <adamhsparks@gmail.com>
2018-08-27 12:27:29 +1000

    Format code and follow bomrang conventions
    - use `call. = FALSE` for stop functions
    
    - wrap lines at 80 chars
    
    - code formatting

--- R/get_radar_imagery.R
+++ R/get_radar_imagery.R
@@ -30,36 +30,40 @@
 #' @author Dean Marchiori, \email{deanmarchiori@@gmail.com}
 #'
 #' @export
-get_available_radar <- function(radar_id = "all"){
+get_available_radar <- function(radar_id = "all") {
   ftp_base <- "ftp://ftp.bom.gov.au/anon/gen/radar/"
-  radar_locations <- NULL
-  load(system.file("extdata", "radar_locations.rda", package = "bomrang")) 
+  radar_locations <- NULL #nocov
+  load(system.file("extdata", "radar_locations.rda", package = "bomrang"))
   list_files <- curl::new_handle()
-  curl::handle_setopt(list_files, ftp_use_epsv = TRUE, dirlistonly = TRUE)
+  curl::handle_setopt(list_files,
+                      ftp_use_epsv = TRUE,
+                      dirlistonly = TRUE)
   con <- curl::curl(url = ftp_base, "r", handle = list_files)
   files <- readLines(con)
   close(con)
   gif_files <- files[grepl("^.*\\.gif", files)]
-  product_id <- substr(gif_files, 1, nchar(gif_files)-4)
+  product_id <- substr(gif_files, 1, nchar(gif_files) - 4)
   LocationID <- substr(product_id, 4, 5)
   range <- substr(product_id, 6, 6)
-  dat <- cbind.data.frame(product_id, 
-                          LocationID, 
-                          range, 
-                          stringsAsFactors = FALSE) %>% 
-    dplyr::left_join(radar_locations, by = "LocationID") %>% 
-    dplyr::mutate(range  = dplyr::case_when(
-      range == 1 ~ "512km",
-      range == 2 ~ "256km",
-      range == 3 ~ "128km",
-      range == 4 ~ "64km")
+  dat <- cbind.data.frame(product_id,
+                          LocationID,
+                          range,
+                          stringsAsFactors = FALSE) %>%
+    dplyr::left_join(radar_locations, by = "LocationID") %>%
+    dplyr::mutate(
+      range  = dplyr::case_when(
+        range == 1 ~ "512km",
+        range == 2 ~ "256km",
+        range == 3 ~ "128km",
+        range == 4 ~ "64km"
+      )
     )
-  if(radar_id[1] == "all"){
+  if (radar_id[1] == "all") {
     dat <- dat
-  }else if(as.numeric(radar_id) %in% dat$Radar_id){
-    dat <- dat[dat$Radar_id %in% as.numeric(radar_id),]
-  }else{
-   stop("radar_id not found")
+  } else if (as.numeric(radar_id) %in% dat$Radar_id) {
+    dat <- dat[dat$Radar_id %in% as.numeric(radar_id), ]
+  } else{
+    stop("radar_id not found")
   }
   return(dat)
 }
@@ -70,7 +74,8 @@ get_available_radar <- function(radar_id = "all"){
 #' \url{ftp://ftp.bom.gov.au/anon/gen/radar/} and return a raster
 #' \code{\link[raster]{raster}} object. Files available are the most recent 
 #' radar snapshot which are updated approximately every 6 to 10 minutes. 
-#' Suggested to check file availability first by using \code{\link{get_available_radar}}.
+#' Suggested to check file availability first by using
+#' \code{\link{get_available_radar}}.
 #'
 #' @param product_id Character.  BOM product ID to download and import as a 
 #' \code{\link[raster]{raster}} object. Value is required.
@@ -102,10 +107,13 @@ get_available_radar <- function(radar_id = "all"){
 #' @export
 get_radar_imagery <- function(product_id = NULL) {
   if (is.null(product_id)) {
-    stop("\nYou must select a valid BOM radar imagery Product ID.\n")
+    stop("\nYou must select a valid BOM radar imagery Product ID.\n",
+         call. = FALSE)
   }
   if (length(product_id) != 1) {
-    stop("\nbomrang only supports working with one Product ID at a time for radar images\n")
+    stop("\nbomrang only supports working with one Product ID at a time",
+        "for radar images\n",
+         call. = FALSE)
   }
   ftp_base <- "ftp://ftp.bom.gov.au/anon/gen/radar"
   fp <- file.path(ftp_base, paste0(product_id, ".gif"))
