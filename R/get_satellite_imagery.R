6518469d58b7a566fbfdcb0fd1c1cdc1b697623c
Adam H. Sparks <adamhsparks@gmail.com>
2018-08-27 19:07:07 +1000

    fail if no product_id specified for satelite imagery
    See comment from @hughparsonage https://github.com/ropensci/bomrang/pull/83#discussion_r212882848

--- R/get_satellite_imagery.R
+++ R/get_satellite_imagery.R
@@ -139,10 +139,10 @@ get_available_imagery <- function(product_id = "all") {
 #' @author Adam H Sparks, \email{adamhsparks@@gmail.com}
 #' @export
 get_satellite_imagery <-
-  function(product_id = NULL,
+  function(product_id,
            scans = 1,
            cache = FALSE) {
-    if (is.null(product_id)) {
+    if (length(product_id) != 1) {
       stop("\nYou must select a valid BOM satellite imagery Product ID.\n")
     }
     if (length(unique(substr(product_id, 1, 8))) != 1) {
