57b62dfcbb9212b9dad8c2332a34d6cac82013a8
Adam H. Sparks <adamhsparks@gmail.com>
2018-09-11 13:36:31 +1000

    Adds tests for meta argument in get_historical()

--- tests/testthat/test-get_historical.R
+++ tests/testthat/test-get_historical.R
@@ -24,6 +24,16 @@ test_that("Query stationid = '023000',
   expect_equal(ADLhistrain$Station_number[1], 23000)
 })
 
+test_that("Query stationid = '023000',
+          meta == TRUE returns a list with metadata and weather data", {
+            skip_on_cran()
+            ADLhistmax <- get_historical("023000", type = "max", meta = TRUE)
+            expect_is(ADLhistmax, "list")
+            expect_equal(length(ADLhistmax), 2)
+            expect_equal(names(ADLhistmax), c("meta", "historical_data"))
+            expect_equal(nrow(ADLhistmax[[1]]), 1)
+          })
+
 test_that("Query latlon = c(-34.9285, 138.6007),
           type = 'rain' returns data.frame w/ correct station and some data", {
   skip_on_cran()
