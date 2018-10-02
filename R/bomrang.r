66536847b5bcc6b39266dc976ed6f0a200ee0330
Adam H. Sparks <adamhsparks@gmail.com>
2018-10-02 15:33:26 +1000

    Removes "From R" from Title

--- R/bomrang.r
+++ R/bomrang.r
@@ -1,5 +1,5 @@
 
-#' Australian Government Bureau of Meteorology (BOM) Data from R
+#' Australian Government Bureau of Meteorology (BOM) Data Client
 #'
 #' @name bomrang
 #' @author Adam H Sparks and Jonathan Carroll and Dean Marchiori and Mark
