0261825cb79b62a0e87109b47c1704ad8dab48e1
Adam H. Sparks <adamhsparks@gmail.com>
2018-10-19 21:53:35 +1000

    Removes redundant paste() in message

--- R/get_radar_imagery.R
+++ R/get_radar_imagery.R
@@ -136,10 +136,10 @@ get_radar_imagery <- function(product_id,
   
   if (download_only == TRUE) {
     curl::curl_download(url = fp, destfile = path, mode = "wb") 
-    message(paste("file downloaded to:", path))
+    message("file downloaded to:", path)
   } else {
     curl::curl_download(url = fp, destfile = path, mode = "wb") 
-    message(paste("file downloaded to:", path))
+    message("file downloaded to:", path)
     y <- raster::raster(x = path)
     y[is.na(y)] <- 999
     return(y)
