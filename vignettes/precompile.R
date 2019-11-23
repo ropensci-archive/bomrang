8a8430235f3939fcf81831ef8df57d34c8cde393
Adam H. Sparks <adamhsparks@gmail.com>
2019-11-23 12:30:27 +1000

    Prebuild vignette

---
+++ vignettes/precompile.R
@@ -0,0 +1,22 @@
+# vignettes that depend on internet access need to be precompiled and take a
+# while to run
+library(knitr)
+knit("vignettes/bomrang.Rmd.orig", "vignettes/bomrang.Rmd")
+
+# remove file path such that vignettes will build with figures
+replace <- readLines("vignettes/bomrang.Rmd")
+replace <- gsub("<img src=\"vignettes/", "<img src=\"", replace)
+fileConn <- file("vignettes/bomrang.Rmd")
+writeLines(replace, fileConn)
+close(fileConn)
+
+# build vignettes
+library(devtools)
+build_vignettes()
+
+# move resource files to /doc
+resources <-
+  list.files("vignettes/", pattern = ".png$", full.names = TRUE)
+file.copy(from = resources,
+          to = "doc",
+          overwrite =  TRUE)
