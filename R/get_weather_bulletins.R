1a062961842c703e1f18bb32e049a2d455a059f4
Adam H. Sparks <adamhsparks@gmail.com>
2020-01-08 09:35:14 +1000

    Add name of Appendix and commas in docs

--- R/get_weather_bulletins.R
+++ R/get_weather_bulletins.R
@@ -29,10 +29,10 @@
 #'
 #' @return
 #' Tidy data frame of Australian 9am or 3pm weather observations for a state.
-#' For full details of fields and units returned see Appendix 4 in the
-#' \pkg{bomrang} vignette, use \cr
-#' \code{vignette("bomrang", package = "bomrang")}
-#' to view.
+#' For full details of fields and units returned see Appendix 4,
+#' "Appendix 4 - Output from get_weather_bulletin()" in the \pkg{bomrang}
+#' vignette, use \cr
+#' \code{vignette("bomrang", package = "bomrang")} to view.
 #'
 #' @examples
 #' \donttest{
