82402b757e47ff5ab0ae1345eff67563d8c54081
Adam H. Sparks <adamhsparks@gmail.com>
2020-01-08 09:38:04 +1000

    Fix documentation formatting and remove extra line

--- R/sweep_for_stations.R
+++ R/sweep_for_stations.R
@@ -3,8 +3,8 @@
 #'
 #' @param latlon A length-2 numeric vector. By default, Canberra
 #' (approximately).
-#' @return A `data.table` of all weather stations (in this package) sorted
-#' by distance from \var{latlon}, ascending.
+#' @return A \code{\link{data.table}} of all weather stations (in this package)
+#' sorted by distance from \var{latlon}, ascending.
 #' @author Hugh Parsonage, \email{hugh.parsonage@@gmail.com}
 #' @importFrom data.table copy setorderv
 #' @export sweep_for_stations
@@ -28,4 +28,3 @@ sweep_for_stations <- function(latlon = c(-35.3, 149.2)) {
     setorderv("distance") %>%
     .[]
 }
-
