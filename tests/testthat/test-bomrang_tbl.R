5063f06cffaa5b0976b5ed1e92b9b8f151920487
Adam H. Sparks <adamhsparks@gmail.com>
2021-03-29 09:36:12 +0800

    Fix tests by removing crayon formatting

--- tests/testthat/test-bomrang_tbl.R
+++ tests/testthat/test-bomrang_tbl.R
@@ -5,14 +5,16 @@ test_that("print.bomrang_tbl() returns a proper header", {
     capture.output(get_historical_weather(latlon = c(-35.2809, 149.1300),
                                           type = "min"))
   expect_type(x, "character")
-  expect_equal(x[[1]],
-               "  --- Australian Bureau of Meteorology (BOM) Data Resource ---")
+  expect_equal(
+    x[[1]],
+    crayon::strip_style("  --- Australian Bureau of Meteorology (BOM) Data Resource ---")
+  )
   expect_equal(x[[2]],
-               "  (Original Request Parameters)")
+               crayon::strip_style("  (Original Request Parameters)"))
   expect_equal(x[[3]],
-               "  Station:\t\tCANBERRA AIRPORT [070351] ")
+               crayon::strip_style("  Station:\t\tCANBERRA AIRPORT [070351] "))
   expect_equal(x[[4]],
-               "  Location:\t\tlat: -35.3088, lon: 149.2004")
+               crayon::strip_style("  Location:\t\tlat: -35.3088, lon: 149.2004"))
   expect_equal(x[[5]],
-               "  Measurement / Origin:\tMin / Historical")
+               crayon::strip_style("  Measurement / Origin:\tMin / Historical"))
 })
